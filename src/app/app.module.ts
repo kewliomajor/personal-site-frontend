4d22febca4f72bb7e0d64d3e70b74bb8edf34dba
Cora Hutchinson <corahutchinson2@gmail.com>
2017-07-04 10:06:57 -0400

    getting basic admin lte demo to work, except control panel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './admin-components/header/header.component';
+import { FooterComponent } from './admin-components/footer/footer.component';
+import { MenuComponent } from './admin-components/menu/menu.component';
+import { SettingsComponent } from './admin-components/settings/settings.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MenuComponent,
+    SettingsComponent
   ],
   imports: [
     BrowserModule
