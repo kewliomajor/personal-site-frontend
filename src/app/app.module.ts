93aa276be7fef203e89b11c2df2cec91fdb4dcf5
Cora Hutchinson <corahutchinson2@gmail.com>
2017-07-08 20:09:33 -0400

    reworking the page so that it isnt just template data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './admin-components/header/header.component';
 import { FooterComponent } from './admin-components/footer/footer.component';
 import { MenuComponent } from './admin-components/menu/menu.component';
 import { SettingsComponent } from './admin-components/settings/settings.component';
+import { PostComponent } from './content/post/post.component';
+import { AboutComponent } from './about/about.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const appRoutes: Routes = [
+  { path: 'content', component: PostComponent },
+  { path: 'about', component: AboutComponent },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
@@ -13,9 +23,16 @@ import { SettingsComponent } from './admin-components/settings/settings.componen
     HeaderComponent,
     FooterComponent,
     MenuComponent,
-    SettingsComponent
+    SettingsComponent,
+    PostComponent,
+    AboutComponent,
+    PageNotFoundComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true }
+    ),
     BrowserModule
   ],
   providers: [],
